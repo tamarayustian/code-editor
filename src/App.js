ef38632e6af394211409fe395a760e7c2e51439d
tamara <tamarayustian@gmail.com>
2022-12-01 16:30:12 +0800

    create css from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState("");
+  const [language, setLanguage] = useState("");
+  const [theme, setTheme] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ControlsBox"></div>
+      <div className="PanelsBox"></div>
     </div>
   );
 }
-
-export default App;
